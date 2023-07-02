9aa70d0678f09d01fbe0649ccce8c7c95a077692
Mario Zechner <badlogicgames@gmail.com>
2023-07-03 00:12:40 +0200

    Fix string escapes.

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const tokenizer = require("./lib/tokenizer");
 
 const source = {
   id: "source",
-  text: `This is a test अ "👨‍👩‍👧🇺🇸😀test" < >= = ([==) 123 123.545 012 0xff 0b101`,
+  text: `This is a test अ "👨‍👩‍👧🇺🇸😀\\rte \\\" \\ns\\tt" < >= = ([==) 123 123.545 012 0xff 0b101`,
 };
 
 const { tokens, errors } = tokenizer.tokenize(source);
