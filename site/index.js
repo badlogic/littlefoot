73259a88b5ba4b72e67afe5039ac0db64e020486
Mario Zechner <badlogicgames@gmail.com>
2023-07-03 00:02:14 +0200

    Fix up playground.

--- site/index.js
+++ site/index.js
@@ -4,10 +4,10 @@ const source = document.querySelector("#source");
 const output = document.querySelector("#output");
 
 source.addEventListener("input", () => {
-  output.innerHTML = tokenizer
-    .tokenize({ id: "source", text: source.value })
-    .map((token) => {
-      JSON.stringify(token, null, 2);
-    })
-    .join("<br>");
+  const { tokens, errors } = tokenizer.tokenize({ id: "source", text: source.value });
+  if (errors.length == 0) {
+    output.innerHTML = tokens.map((token) => JSON.stringify(token, null, 2)).join("<br>");
+  } else {
+    output.innerHTML = errors.map((error) => JSON.stringify(error, null, 2)).join("<br>");
+  }
 });
