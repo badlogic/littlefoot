dff649414be758f2078d413b8980dc6bdb6522f9
Mario Zechner <badlogicgames@gmail.com>
2023-07-02 23:58:26 +0200

    Number tokenization.

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const tokenizer = require("./lib/tokenizer");
 
 const source = {
   id: "source",
-  text: `This is a test अ "👨‍👩‍👧🇺🇸😀test" < >= = ([==)`,
+  text: `This is a test अ "👨‍👩‍👧🇺🇸😀test" < >= = ([==) 123 123.545 012 0xff 0b101`,
 };
 
 const { tokens, errors } = tokenizer.tokenize(source);
