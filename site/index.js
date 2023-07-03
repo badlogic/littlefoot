f8cecd28ed6b5d7866fd78b0127bf7b8475d4071
Mario Zechner <badlogicgames@gmail.com>
2023-07-03 22:26:53 +0200

    Error highlighting.

--- site/index.js
+++ site/index.js
@@ -3,11 +3,26 @@ const { tokenizer } = require("../lib");
 const source = document.querySelector("#source");
 const output = document.querySelector("#output");
 
-source.addEventListener("input", () => {
-  const { tokens, errors } = tokenizer.tokenize({ id: "source", text: source.value });
+function tokenize(value) {
+  localStorage.setItem("source", value);
+  const sourceDoc = new tokenizer.Source("source", value);
+  const { tokens, errors } = tokenizer.tokenize(sourceDoc);
   if (errors.length == 0) {
     output.innerHTML = tokens.map((token) => JSON.stringify(token, null, 2)).join("<br>");
   } else {
-    output.innerHTML = errors.map((error) => JSON.stringify(error, null, 2)).join("<br>");
+    output.innerHTML = sourceDoc
+      .printErrors(errors)
+      .reduce((prev, curr) => prev + curr)
+      .replace(/\n/g, "<br>");
   }
+}
+
+const stored = localStorage.getItem("source");
+if (stored) {
+  source.value = stored;
+  tokenize(source.value);
+}
+
+source.addEventListener("input", () => {
+  tokenize(source.value);
 });
