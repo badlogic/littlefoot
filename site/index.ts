c8e7cf97e0e42dd06c9ced60210ea713f6ea07f2
Mario Zechner <badlogicgames@gmail.com>
2023-07-07 22:16:23 +0200

    Error highlighting in Monaco editor

--- site/index.ts
+++ site/index.ts
@@ -1,28 +1,10 @@
 import "monaco-editor/min/vs/editor/editor.main.css";
 import * as monaco from "monaco-editor";
-import { parse, Source } from "../lib";
+import { LittleFootError, parse, Source } from "../lib";
 
 const editorContainer = document.querySelector("#editor") as HTMLTextAreaElement;
 const source = document.querySelector("#source") as HTMLTextAreaElement;
 const output = document.querySelector("#output") as HTMLDivElement;
-
-function tokenize(value: string) {
-  localStorage.setItem("source", value);
-  const sourceDoc = new Source("source", value);
-  const { ast, errors } = parse(sourceDoc);
-  if (errors.length == 0) {
-    output.innerHTML = JSON.stringify(
-      ast,
-      (key, value) => (key == "source" || key == "firstToken" || key == "lastToken" ? undefined : value),
-      2
-    ).replace(/\n/g, "<br>");
-  } else {
-    output.innerHTML = errors
-      .map((error) => error.toString())
-      .reduce((prev, curr) => prev + curr)
-      .replace(/\n/g, "<br>");
-  }
-}
 
 (globalThis as any).self.MonacoEnvironment = {
   getWorkerUrl: function (moduleId: any, label: any) {
@@ -47,3 +29,40 @@ editor.onDidChangeModelContent(() => {
 const stored = localStorage.getItem("source");
 if (stored) editor.setValue(stored);
 tokenize(editor.getValue());
+
+function tokenize(value: string) {
+  localStorage.setItem("source", value);
+  const sourceDoc = new Source("source", value);
+  const { ast, errors } = parse(sourceDoc);
+  if (errors.length == 0) {
+    highlightErrors([]);
+    output.innerHTML = JSON.stringify(
+      ast,
+      (key, value) => (key == "source" || key == "firstToken" || key == "lastToken" ? undefined : value),
+      2
+    ).replace(/\n/g, "<br>");
+  } else {
+    highlightErrors(errors);
+    output.innerHTML = errors
+      .map((error) => error.toString())
+      .reduce((prev, curr) => prev + curr)
+      .replace(/\n/g, "<br>");
+  }
+}
+
+function highlightErrors(errors: LittleFootError[]) {
+  const markers: monaco.editor.IMarkerData[] = [];
+  errors.forEach((error) => {
+    const startPos = editor.getModel()!.getPositionAt(error.start);
+    const endPos = editor.getModel()!.getPositionAt(error.end);
+    markers.push({
+      severity: monaco.MarkerSeverity.Error,
+      startLineNumber: startPos.lineNumber,
+      startColumn: startPos.column,
+      endLineNumber: endPos.lineNumber,
+      endColumn: endPos.column,
+      message: error.message,
+    });
+  });
+  monaco.editor.setModelMarkers(editor.getModel()!, "littlefoot", markers);
+}
