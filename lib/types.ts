5a4a370750a5bbba48c17cbf0ea4a53ad0cdd5c9
Mario Zechner <badlogicgames@gmail.com>
2023-07-11 20:13:00 +0200

    Fix type isEqual and isAssignableTo, add comments.

--- lib/types.ts
+++ lib/types.ts
@@ -216,19 +216,26 @@ export class Types {
 }
 
 function isEqual(a: Type, b: Type) {
+  // Unpack the type of named types.
   if (a.kind == "named function" || a.kind == "named type") {
     a = a.type;
   }
   if (b.kind == "named function" || b.kind == "named type") {
     b = b.type;
   }
+
   if (a.kind == "primitive" && b.kind == "primitive") {
+    // Primitive types must match by name exactly.
     return a.name == b.name;
   } else if (a.kind == "list" && b.kind == "list") {
+    // List types must have equal element types.
     return isEqual(a.elementType, b.elementType);
   } else if (a.kind == "map" && b.kind == "map") {
+    // Map types must have equal value types.
     return isEqual(a.valueType, b.valueType);
   } else if (a.kind == "record" && b.kind == "record") {
+    // Records must have the same number of fields. Each
+    // pair of fields must match in both name and type.
     if (a.fields.length != b.fields.length) return false;
     for (let i = 0; i < a.fields.length; i++) {
       const aField = a.fields[i];
@@ -238,6 +245,19 @@ function isEqual(a: Type, b: Type) {
     }
     return true;
   } else if (a.kind == "union" && b.kind == "union") {
+    // Unions must have the same number of types. Each type
+    // of a must be found in b.
+    //
+    // FIXME the current implementation assumes that all types
+    // within a union are distinct. However, the following
+    // degenerate case can arise (e.g. when mixins are involved):
+    //
+    // type a = someType | someType | someType
+    // type b = someType | anotherType | anotherType
+    //
+    // These two types are not equal, even though all types
+    // of a can be found in b. The current implementation will
+    // report these two types as equal.
     if (a.types.length != b.types.length) return false;
     for (let i = 0; i < a.types.length; i++) {
       const aType = a.types[i];
@@ -252,30 +272,54 @@ function isEqual(a: Type, b: Type) {
       if (!found) return false;
     }
     return true;
+  } else if (a.kind == "function" && b.kind == "function") {
+    // Functions must have the same number of parameters. Each
+    // parameter pair must have equal types. The return types
+    // must also be equal. The parameter names
+    // are irrelevant for equality.
+    if (a.parameters.length != b.parameters.length) return false;
+    for (let i = 0; i < a.parameters.length; i++) {
+      if (!isEqual(a.parameters[i].type, b.parameters[i].type)) return false;
+    }
+    if (!isEqual(a.returnType, b.returnType)) return false;
+    return true;
   } else {
     return false;
   }
 }
 
-function canAssignTo(a: Type, b: Type) {
+function isAssignableTo(a: Type, b: Type) {
   if (a.kind == "named function" || a.kind == "named type") {
     a = a.type;
   }
   if (b.kind == "named function" || b.kind == "named type") {
     b = b.type;
   }
+
+  // This handles primitives and is also an early out for
+  // exact type matches. This also handles exact record matches
+  // Non-exact matches are handled below.
   if (isEqual(a, b)) return true;
+
   if (b.kind == "union") {
+    // If a is a union and b is not, it can not be assigned.
+    // This is why we start by check if b is a union.
+    //
+    // There are two cases:
+    // 1. If a is not a union, then it must be assignable
+    //    to at least one type in the union.
+    // 2. If a is a union, then all of a's types must be
+    //    assignable to at least one type in b.
     if (a.kind != "union") {
       for (const type of b.types) {
-        if (canAssignTo(type, a)) return true;
+        if (isAssignableTo(type, a)) return true;
       }
       return false;
     } else {
       for (const aType of a.types) {
         let found = false;
         for (const bType of b.types) {
-          if (canAssignTo(aType, bType)) {
+          if (isAssignableTo(aType, bType)) {
             found = true;
             break;
           }
@@ -285,26 +329,49 @@ function canAssignTo(a: Type, b: Type) {
       return true;
     }
   } else if (a.kind == "list" && b.kind == "list") {
-    return canAssignTo(a.elementType, b.elementType);
+    // For lists, the element type of a must be assignable to
+    // element type of b.
+    return isAssignableTo(a.elementType, b.elementType);
   } else if (a.kind == "map" && b.kind == "map") {
-    return canAssignTo(a.valueType, b.valueType);
+    // For maps, the value type of a must be assignable to
+    // value types of b.
+    return isAssignableTo(a.valueType, b.valueType);
   } else if (a.kind == "record" && b.kind == "record") {
     // We only get here if isEqual(a, b) was false, which means
     // the two record types don't have the same number or exact
     // types of fields.
+    //
     // However, if we can assign all fields of a to a subset of fields
     // of record b, we can assign a to b.
     for (const aField of a.fields) {
       let found = false;
       for (const bField of b.fields) {
-        if (canAssignTo(aField.type, bField.type)) {
+        if (isAssignableTo(aField.type, bField.type)) {
           found = true;
           break;
         }
       }
       if (!found) return false;
     }
+  } else if (a.kind == "function" && b.kind == "function") {
+    // For functions, the number of parameters must match,
+    // and the parameter types of a must be assignable to
+    // the parameter types of b. The return type of a must
+    // also be assignable to b. The parameter names do not matter.
+    if (a.parameters.length != b.parameters.length) return false;
+    for (let i = 0; i < a.parameters.length; i++) {
+      if (!isAssignableTo(a.parameters[i].type, b.parameters[i].type)) return false;
+    }
+    if (!isAssignableTo(a.returnType, b.returnType)) return false;
+    return true;
   } else {
     return false;
   }
 }
+
+type a = (a: number) => string | number;
+type b = (a: number) => string;
+
+type AreEqual<T, U> = T extends U ? (U extends T ? true : false) : false;
+
+type typesEqual = AreEqual<a, b>;
